9f038d4f5bbaaca87f8a0f45dace9da31a9806d6
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-05-15 18:53:33 +0200

    #17: merge master

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,25 +14,42 @@ class CoursesController < ApplicationController
   end
 
   def create
-    @course = Course.create(course_params)
-    redirect_to home_path, success: "Course successfully created"
+    @course = current_teacher.courses.build(course_params)
+    if @course.save
+      redirect_to root_path, success: "Course successfully created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
   end
 
   def update
     @course = Course.find(params[:id])
     @course.update(course_params)
-    redirect_to home_path, success: "Course successfully updated"
+    redirect_to root_path, success: "Course successfully updated"
   end
 
   def destroy
     @course = Course.find(params[:id])
     @course.destroy
-    redirect_to home_path, success: "Course successfully deleted"
+    redirect_to root_path, success: "Course successfully deleted"
+=begin
+    ArgumentError in CoursesController#destroy
+    wrong number of arguments (given 0, expected 1)
+    Extracted source (around line #5):
+    class Step < ApplicationRecord
+      belongs_to :course
+      has_many :achievements, dependent: destroy <=======================================================
+    end
+=end
   end
 
   private
 
   def course_params
-    params.require(:course).permit(:title, :content)
+    params.require(:course).permit(:title, :description)
   end
 end
