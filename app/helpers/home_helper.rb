ca55de2c8f3482930489fcb99b5571ee22667307
Laspargus <maxime.speroni@gmail.com>
2019-06-10 19:19:12 +0200

    home and devise sign up and sign in

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module HomeHelper
+
+    def resource_name
+    :teacher
+  end
+
+  def resource
+    @resource ||= Teacher.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:teacher]
+  end
 end
