76c5c53fdaa6ce582c3be5d3cbcbc10702b17da6
olivierdumas <dumas.olivier@outlook.fr>
2019-06-11 15:21:06 +0200

    clean nav and view

--- app/javascript/src/components/AttendanceInvited.jsx
+++ app/javascript/src/components/AttendanceInvited.jsx
@@ -19,7 +19,7 @@ export default class AttendanceInvited extends React.Component {
     const invited = attendances.filter(attendance => attendance.status === false);
     return (
       <div>
-        <span>Students Invited</span><hr />
+        <span>Students Invited ({invited.length})</span><hr />
         {invited.map(attendance => (
           <div key={attendance.id}>
             {attendance.student.email}<button className="m-2 btn btn-danger" value={attendance.id} onClick={this.handleRemoveClick}><i className="far fa-trash-alt"></i></button>          
