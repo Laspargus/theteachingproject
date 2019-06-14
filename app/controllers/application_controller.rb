b50b816b68a9687444276a0dcce02dc59132d952
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-06-14 17:34:04 +0200

    add sentry to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_raven_context
 
   before_action :sanitize_devise_params, if: :devise_controller?
 
@@ -13,4 +14,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || courses_path
   end
+
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: session[:current_user_id]) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+  
 end
